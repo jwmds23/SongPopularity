00a53d6875d53d65c45f4d9d3e9dd29f1b405c6c
unknown <willxu2023@outlook.com>
2024-02-26 11:00:38 -0800

    add code for graph and fix bugs

--- src/will_try.py
+++ src/will_try.py
@@ -1,7 +1,7 @@
 import altair as alt
 import pandas as pd
-import matplotlib as plt
 import numpy as np
+import plotly.graph_objs as go
 from dash import Dash, html, dcc
 from dash.dependencies import Input, Output, State
 from sklearn.ensemble import RandomForestRegressor
@@ -24,45 +24,43 @@ tempo_scalar_mm = load('support_model/tempo_scalar_mm.joblib')
 
 key_options = [{'label': str(i), 'value': i} for i in range(12)]
 
-def track_radar(danceability, energy, key, loudness, mode, speechiness, acousticness, instrumentalness, liveness, valence, tempo, duration, loud_scalar_mm, dur_scalar_mm, tempo_scalar_mm):
-    # initialize the value
+def track_radar(danceability, energy, key, loudness, mode, speechiness, acousticness, instrumentalness, liveness, valence, tempo, duration, key_scalar_mm=key_scalar_mm, loud_scalar_mm=loud_scalar_mm, dur_scalar_mm=dur_scalar_mm, tempo_scalar_mm=tempo_scalar_mm):
     categories = ['Danceability', 'Energy', 'Key', 'Loudness', 'Mode', 'Speechiness', 'Acousticness', 'Instrumentalness', 'Liveness', 'Valence', 'Tempo', 'Duration']
-    N = len(categories)
-    danceability = danceability
-    energy = energy
-    key = key
-    loudness = loud_scalar_mm.transform(pd.DataFrame({'loudness': [loudness]}))[0][0]
-    mode = mode
-    speechiness = speechiness
-    acousticness = acousticness
-    instrumentalness = instrumentalness
-    liveness = liveness
-    valence = valence
-    tempo = tempo_scalar_mm.transform(pd.DataFrame({'tempo': [tempo]}))[0][0]
-    duration = dur_scalar_mm.transform(pd.DataFrame({'duration_ms': [duration]}))[0][0]
-    values = [danceability, energy, key, loudness, mode, speechiness, acousticness, instrumentalness, liveness, valence, tempo, duration]
-
-    # repeat the first value in values to close the circle
-    values += values[:1]
-
-    # calculate the angle for each category
-    angles = [n / float(N) * 2 * np.pi for n in range(N)]
-    angles += angles[:1]
-
-    # plot the radar graph
-    fig, ax = plt.subplots(figsize=(6, 6), subplot_kw=dict(polar=True))
-
-    # Draw one axe per variable + add labels
-    plt.xticks(angles[:-1], categories)
-
-    # Plot each entity
-    ax.plot(angles, values, linewidth=1, linestyle='solid')
-    ax.fill(angles, values, 'b', alpha=0.1)
-
+    
+    # Apply scaling
+    key_scaled = key_scalar_mm.transform(pd.DataFrame({'key': [key]}))[0][0]
+    loudness_scaled = loud_scalar_mm.transform(pd.DataFrame({'loudness': [loudness]}))[0][0]
+    tempo_scaled = tempo_scalar_mm.transform(pd.DataFrame({'tempo': [tempo]}))[0][0]
+    duration_scaled = dur_scalar_mm.transform(pd.DataFrame({'duration_ms': [duration]}))[0][0]
+    
+    # Organize values
+    values_scaled = [danceability, energy, key_scaled, loudness_scaled, mode, speechiness, acousticness, instrumentalness, liveness, valence, tempo_scaled, duration_scaled]
+    values = values_scaled + [values_scaled[0]]  # Close the radar chart
+    
+    fig = go.Figure(data=[
+        go.Scatterpolar(
+            r=values,
+            theta=categories,
+            fill='toself',
+            name='Track Features'
+        )
+    ])
+    
+    fig.update_layout(
+        polar=dict(
+            radialaxis=dict(
+                visible=True,
+                range=[0, 1]
+            )
+        ),
+        showlegend=False
+    )
+    
     return fig
 
-def pop_predict(type, danceability, energy, key, loudness, mode, speechiness, acousticness, instrumentalness, liveness, valence, tempo, duration, loud_scalar, tempo_scalar, dur_scalar, model):
-    # standardize loud, tempo and duartion
+def pop_predict(type, danceability, energy, key, loudness, mode, speechiness, acousticness, instrumentalness, liveness, valence, tempo, duration, key_scalar=key_scalar, loud_scalar=loud_scalar, tempo_scalar=tempo_scalar, dur_scalar=dur_scalar, model=model):
+    # standardize key, loud, tempo and duartion
+    key = key_scalar.transform(pd.DataFrame({'key': [loudness]}))[0][0]
     loudness = loud_scalar.transform(pd.DataFrame({'loudness': [loudness]}))[0][0]
     tempo = tempo_scalar.transform(pd.DataFrame({'tempo': [tempo]}))[0][0]
     duration = dur_scalar.transform(pd.DataFrame({'duration_ms': [duration]}))[0][0]
@@ -92,24 +90,6 @@ def pop_predict(type, danceability, energy, key, loudness, mode, speechiness, ac
     pred_df = pd.concat([dummy_df, pred_df], axis=1)
 
     return model.predict(pred_df)[0]
-# the func to generate 'Average Popularity Trend' Graph
-# def pop_trend(df, start, end):
-#     new_df = df[df['track_album_release_date'].between(start, end)].loc[:,['track_album_release_date', 'track_popularity']]
-#     new_df = new_df.groupby(new_df.track_album_release_date.dt.year).mean(numeric_only=True)
-#     new_df = new_df.reset_index()
-#     chart = alt.Chart(
-#             new_df, title=alt.TitleParams('Average Popularity Trend', fontSize=22)
-#             ).mark_line().encode(
-#             x = alt.X('track_album_release_date', axis=alt.Axis(format='Y', grid=False, tickCount=5, titleFontSize=16, labelFontSize=12), title='Release Year'),
-#             y = alt.Y('track_popularity', axis=alt.Axis(titleFontSize=16, labelFontSize=12), scale=alt.Scale(zero=False), title='Popularity'),
-#             color = alt.value('orange')
-#         ).configure_view(
-#             stroke=None
-#         ).properties(
-#             width=600,
-#             height=400
-#         )
-#     return chart.to_html()
 
 app = Dash(__name__)
 
@@ -209,12 +189,14 @@ app.layout = html.Div([
             max=59
         ),
         html.Button('Apply', id = 'apply', n_clicks = 0),
-        html.Div(id='test')
+        html.Div(id='test1'),
+        dcc.Graph(id='test2')
     ])
 ])
 
 @app.callback(
-    Output('test', 'children'),
+    [Output('test1', 'children'),
+     Output('test2', 'figure')],
     [Input('apply', 'n_clicks')],
     [State('genre', 'value'),
      State('danceability', 'value'),
@@ -234,39 +216,12 @@ app.layout = html.Div([
 )
 def update_output(n_clicks, genre, danceability, energy, key, loudness, mode, speechiness, acousticness, instrumentalness, livenss, valence, tempo, minutes, seconds):
     if n_clicks > 0:
-        return f"Result{genre, danceability, energy, key, loudness, mode, speechiness, acousticness, instrumentalness, livenss, valence, tempo, minutes, seconds}"
+        duration_ms = minutes * 6e+7 + seconds * 1e+6   # transfer the time to microseconds
+        test1_output = round(pop_predict(genre, danceability, energy, key, loudness, mode, speechiness, acousticness, instrumentalness, livenss, valence, tempo, duration_ms), 2)
+        test2_output = track_radar(danceability, energy, key, loudness, mode, speechiness, acousticness, instrumentalness, livenss, valence, tempo, duration_ms)
+        return test1_output, test2_output
     else:
-        return ''
-# def update_output(type):
-#     return pop_predict(type)
-#     html.Label('Release Date'),
-#     html.Div([dcc.DatePickerRange(
-#         id = 'date-select',
-#         display_format = 'YYYY-MM-DD',
-#         start_date = df.track_album_release_date.min().strftime('%Y-%m-%d'),
-#         end_date = df.track_album_release_date.max().strftime('%Y-%m-%d'),
-#         start_date_placeholder_text='Start date',
-#         end_date_placeholder_text='End date',
-#         minimum_nights=1800,
-#         stay_open_on_select=True,
-#         clearable=True
-#     )]),
-#     html.Iframe(
-#         id='average-pop-trend',
-#         srcDoc=pop_trend(df, start=df.track_album_release_date.min(), end=df.track_album_release_date.max()),
-#         style={'width':'100%', 'height':'800px'}
-#     )
-# ])
-
-# @app.callback(
-#     Output('average-pop-trend', 'srcDoc'),
-#     [Input('date-select', 'start_date'),
-#      Input('date-select', 'end_date')]
-# )
-# def update_output(start, end):
-#     return pop_trend(df, start, end)
-
-
+        return '', track_radar(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
 
 # Run the app
 if __name__ == '__main__':
